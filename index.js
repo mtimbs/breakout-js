7fb61898b85c4baaf6ca044701414ece7ddf39e8
Michael Timbs <hello@michaeltimbs.me>
2024-07-31 23:24:58 +0930

    style lives and score. mior cleanup

--- index.js
+++ index.js
@@ -1,61 +1,75 @@
+let customFont = new FontFace("CustomFont", "url(assets/QuinqueFive.woff)");
+
 const canvas = document.getElementById("app");
-if (window.innerWidth < 2 * window.innerHeight) {
+
+const NATIVE_WIDTH = 400;
+const NATIVE_HEIGHT = 300;
+const NATIVE_RATIO = NATIVE_WIDTH / NATIVE_HEIGHT;
+
+if (window.innerWidth < window.innerHeight * NATIVE_RATIO) {
   canvas.width = window.innerWidth;
-  canvas.height = window.innerWidth / 2;
+  canvas.height = window.innerWidth / NATIVE_RATIO;
 } else {
-  canvas.width = window.innerHeight * 2;
+  canvas.width = window.innerHeight * NATIVE_RATIO;
   canvas.height = window.innerHeight;
 }
-const ctx = canvas.getContext("2d");
 
-const NATIVE_WIDTH = 1024;
-const NATIVE_HEIGHT = 720;
 const SCALING_FACTOR = canvas.width / NATIVE_WIDTH;
+const TOP_PADDING = canvas.height > 650 ? 50 : 0.075 * canvas.height;
+const INITIAL_DX = 0.01 * canvas.width;
+const INITIAL_DY = 0.0125 * canvas.height;
 
-// Load assets
-const image = new Image();
-image.src = "assets/Breakout_Tile_Free.png";
+const BALL_RADIUS = 0.0075 * canvas.width;
 
-const PADDLE_HEIGHT = 15 * SCALING_FACTOR;
-const PADDLE_WIDTH = 90 * SCALING_FACTOR;
-
-// Ball
-const BALL_RADIUS = 8 * SCALING_FACTOR;
-let x = canvas.width / 2;
-let y = canvas.height - 2 * BALL_RADIUS - 0.5 * PADDLE_HEIGHT;
-
-// Ball Rate of Change
-const INITIAL_DX = 5 * SCALING_FACTOR;
-const INITIAL_DY = 5 * SCALING_FACTOR;
-let dx = INITIAL_DX;
-let dy = -INITIAL_DY;
-
-const SPRITE_MAP = {
-  ball: [1403, 652, 64, 64],
-  bricks: [
-    [772, 260, 384, 128],
-    [386, 390, 384, 128],
-  ],
-  paddle: [
-    [1158, 462, 243, 64],
-    [1158, 528, 243, 64],
-    [1158, 594, 243, 64],
-  ],
-};
+const PADDLE_HEIGHT = 0.03 * NATIVE_HEIGHT * SCALING_FACTOR;
+const PADDLE_WIDTH = 0.125 * canvas.width;
 
 const BRICK_ROW_COUNT = 2;
-const BRICK_COLUMN_COUNT = 10;
-const BRICK_PADDING = 2 * SCALING_FACTOR;
-const BRICK_OFFSET_TOP = 2 * BALL_RADIUS * SCALING_FACTOR;
-const BRICK_OFFSET_SIDE = 15 * SCALING_FACTOR;
+const BRICK_COLUMN_COUNT = 15;
+const BRICK_PADDING = 0.0015 * canvas.width;
+const BRICK_OFFSET_TOP = 4 * BALL_RADIUS;
+const BRICK_OFFSET_SIDE = 2 * BALL_RADIUS;
 const room_for_bricks =
-  SCALING_FACTOR * NATIVE_WIDTH -
+  canvas.width -
   2 * BRICK_OFFSET_SIDE -
   (BRICK_COLUMN_COUNT - 1) * BRICK_PADDING;
 
 const BRICK_WIDTH = room_for_bricks / BRICK_COLUMN_COUNT;
 
-const BRICK_HEIGHT = BRICK_WIDTH / 4;
+const BRICK_HEIGHT = BRICK_WIDTH / 3;
+
+// Load assets
+const image = new Image();
+image.src = "assets/Breakout_Tile_Free.png";
+const SPRITE_MAP = {
+  ball: [1403, 652, 64, 64],
+  bricks: [
+    [772, 520, 384, 128], // border
+    [772, 260, 384, 128], // Red
+    [386, 390, 384, 128], // Yellow
+  ],
+  paddle: [
+    [1158, 462, 243, 64],
+    [1158, 462, 243, 64],
+    [1158, 462, 243, 64],
+    [1158, 528, 243, 64],
+    [1158, 528, 243, 64],
+    [1158, 594, 243, 64],
+    [1158, 594, 243, 64],
+    [1158, 594, 243, 64],
+  ],
+  lives: [1637, 652, 64, 58],
+};
+
+const ctx = canvas.getContext("2d");
+
+// Ball
+let x = canvas.width / 2;
+let y = canvas.height - 2 * BALL_RADIUS - 0.5 * PADDLE_HEIGHT;
+
+// Ball Rate of Change
+let dx = INITIAL_DX;
+let dy = -INITIAL_DY;
 
 function drawBall() {
   const [sx, sy, sw, sh] = SPRITE_MAP.ball;
@@ -73,16 +87,20 @@ function drawBall() {
 }
 
 function moveBall() {
-  if (x + dx > canvas.width - BALL_RADIUS || x + dx < BALL_RADIUS) {
+  const rightEdge = x + BALL_RADIUS;
+  const leftEdge = x - BALL_RADIUS;
+
+  if (rightEdge + dx > canvas.width || leftEdge + dx < 0) {
     dx = -dx;
   }
 
-  if (y + dy < BALL_RADIUS) {
+  const bottomEdge = y + BALL_RADIUS;
+  const topEdge = y - BALL_RADIUS;
+
+  if (topEdge + dy < TOP_PADDING + BORDER_BRICK_HEIGHT) {
     dy = -dy;
-  } else if (y + dy > canvas.height - BALL_RADIUS - PADDLE_HEIGHT) {
-    const leftEdge = x - BALL_RADIUS;
-    const rightEdge = x + BALL_RADIUS;
-    if (rightEdge > paddle_x && leftEdge < paddle_x + PADDLE_WIDTH) {
+  } else if (bottomEdge + dy > canvas.height - PADDLE_HEIGHT) {
+    if (rightEdge + dx > paddle_x && leftEdge + dx < paddle_x + PADDLE_WIDTH) {
       dy = -dy;
     }
   }
@@ -98,10 +116,9 @@ let paddle_x = (canvas.width - PADDLE_WIDTH) / 2.0;
 let rightPressed = false;
 let leftPressed = false;
 
-let f = 0;
 let p = 0;
 function drawPaddle() {
-  const [sx, sy, sw, sh] = SPRITE_MAP.paddle[p % 3];
+  const [sx, sy, sw, sh] = SPRITE_MAP.paddle[p % 8];
   ctx.drawImage(
     image,
     sx,
@@ -113,27 +130,22 @@ function drawPaddle() {
     PADDLE_WIDTH,
     PADDLE_HEIGHT,
   );
-  f++;
-  if (f % 5 == 0) {
-    f = 0;
-    p++;
-  }
+  p++;
 }
 
-const PADDLE_DISTANCE_PER_FRAME = 20 * SCALING_FACTOR;
+let paddle_distance_per_frame = 0.25 * PADDLE_WIDTH;
 function movePaddle() {
   if (rightPressed) {
     paddle_x = Math.min(
-      paddle_x + PADDLE_DISTANCE_PER_FRAME,
+      paddle_x + paddle_distance_per_frame,
       canvas.width - PADDLE_WIDTH,
     );
   } else if (leftPressed) {
-    paddle_x = Math.max(paddle_x - PADDLE_DISTANCE_PER_FRAME, 0);
+    paddle_x = Math.max(paddle_x - paddle_distance_per_frame, 0);
   }
 }
 
 // Bricks
-
 const bricks = [];
 for (let c = 0; c < BRICK_COLUMN_COUNT; c++) {
   bricks[c] = [];
@@ -149,11 +161,12 @@ function drawBricks() {
         continue;
       }
       const brick_x = c * (BRICK_WIDTH + BRICK_PADDING) + BRICK_OFFSET_SIDE;
-      const brick_y = r * (BRICK_HEIGHT + BRICK_PADDING) + BRICK_OFFSET_TOP;
+      const brick_y =
+        r * (BRICK_HEIGHT + BRICK_PADDING) + BRICK_OFFSET_TOP + TOP_PADDING;
       bricks[c][r].x = brick_x;
       bricks[c][r].y = brick_y;
 
-      const [sx, sy, sw, sh] = SPRITE_MAP.bricks[r];
+      const [sx, sy, sw, sh] = SPRITE_MAP.bricks[r + 1];
       ctx.drawImage(
         image,
         sx,
@@ -240,18 +253,37 @@ function collisionDetection() {
 // Score
 let score = 0;
 
+const FONT_SIZE = 0.5 * TOP_PADDING;
 function drawScore() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Score: ${score}`, 8, 20);
+  ctx.font = `${FONT_SIZE}px CustomFont`;
+  ctx.fillStyle = "#EEE";
+  ctx.fillText(
+    String(score).padStart(4, "0"),
+    BRICK_OFFSET_SIDE,
+    1.25 * FONT_SIZE,
+  );
 }
 
 // Lives
 let lives = 3;
+const HEART_WIDTH = 0.6 * TOP_PADDING;
+const HEART_PADDING = 0.1 * HEART_WIDTH;
+
 function drawLives() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
+  const [sx, sy, sw, sh] = SPRITE_MAP.lives;
+  for (let i = lives; i > 0; i--) {
+    ctx.drawImage(
+      image,
+      sx,
+      sy,
+      sw,
+      sh,
+      canvas.width - i * (HEART_WIDTH + HEART_PADDING) - BRICK_OFFSET_SIDE,
+      0.2 * TOP_PADDING,
+      HEART_WIDTH,
+      HEART_WIDTH,
+    );
+  }
 }
 
 function checkGameOver() {
@@ -268,7 +300,7 @@ function checkGameOver() {
     (rightEdge < paddle_x || leftEdge > paddle_x + PADDLE_WIDTH)
   ) {
     lives -= 1;
-    x = canvas.width / 2;
+    x = canvas.width * 0.5;
     y = canvas.height - 2 * BALL_RADIUS - 0.5 * PADDLE_HEIGHT;
     dy = -INITIAL_DY;
   }
@@ -280,13 +312,34 @@ function checkGameOver() {
   }
 }
 
+const BORDER_BRICK_WIDTH = canvas.width * 0.05;
+const BORDER_BRICK_HEIGHT = 0.25 * BORDER_BRICK_WIDTH;
+function drawBorderBricks() {
+  const [sx, sy, sw, sh] = SPRITE_MAP.bricks[0];
+  for (let i = 0; i < 20; i++) {
+    ctx.drawImage(
+      image,
+      sx,
+      sy,
+      sw,
+      sh,
+      i * canvas.width * 0.05,
+      TOP_PADDING,
+      BORDER_BRICK_WIDTH,
+      BORDER_BRICK_HEIGHT,
+    );
+  }
+}
+
 function keyDownHandler(e) {
   if (e.key === "Right" || e.key === "ArrowRight") {
     rightPressed = true;
   } else if (e.key === "Left" || e.key === "ArrowLeft") {
     leftPressed = true;
   } else if (e.key === " ") {
-    gameLoop();
+    start(true);
+  } else if (e.key === "Escape") {
+    cancelAnimationFrame(frame);
   }
 }
 
@@ -298,8 +351,10 @@ function keyUpHandler(e) {
   }
 }
 
-function gameLoop() {
+let frame = undefined;
+function start() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBorderBricks();
   drawBricks();
   drawPaddle();
   drawBall();
@@ -309,18 +364,14 @@ function gameLoop() {
   drawScore();
   drawLives();
   checkGameOver();
-  requestAnimationFrame(gameLoop);
-}
-
-function startGame() {
-  requestAnimationFrame(gameLoop);
+  frame = requestAnimationFrame(start);
 }
 
 // Initialise canvas
-drawBricks();
-drawPaddle();
-drawBall();
-drawScore();
-drawLives();
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+customFont.load().then((font) => {
+  document.fonts.add(font);
+  start();
+  cancelAnimationFrame(frame);
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+});
